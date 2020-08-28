358b21d8d53edfc75f69ac770c89b56961ee3c14
Camila Rori <camila.a.rori@gmail.com>
2020-08-27 21:33:23 -0300

    Segundo commit

---
+++ src/app/componentes/nuevocomponente/nuevocomponente.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevocomponente',
+  templateUrl: './nuevocomponente.component.html',
+  styleUrls: ['./nuevocomponente.component.css']
+})
+export class NuevocomponenteComponent implements OnInit {
+
+  public ejemploVar = 'Hola mundo';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
